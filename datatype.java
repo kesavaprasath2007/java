0d2da708d79c171c0d94816ab0bbdb18f70dafa0
kesavaprasath2007 <kesavaprasath72@gmail.com>
2025-09-17 21:58:56 +0530

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,25 @@
+class Geeks
+{
+    public static void main (String args[])
+    {
+        char a = 'G';
+        
+        int i =89;
+        
+        byte b=4;
+        
+        short s =56;
+        double d =4.355453532;
+        float f=4.73333434f;
+        long l = 121211;
+        
+        System.out.println("char;"+a);
+        System.out.println("integer:"+i);
+        System.out.println("byte:"+b);
+        System.out.println("short:"+s);
+        System.out.println("float:"+d);
+        System.out.println("double:"+d);
+        System.out.println("long;"+l);
+    
+    }
+}
